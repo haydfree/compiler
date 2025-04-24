2d7f5e15a0e01c5b680e4cb9f74280d40a9fbc93
Hayden Freeman <haydenfreeman@yahoo.com>
2025-04-24 10:24:50 -0500

    AUTO COMMIT: 2025-04-24 10:24:50

--- main.c
+++ main.c
@@ -3,7 +3,89 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+#define LEXEME_STR_SIZE 64
+
+typedef struct Lexeme
+{
+    char *str;
+    size_t str_len, start_idx, end_idx, line, column;
+    struct Lexeme *last, *next;
+} Lexeme;
+
+Lexeme*
+init_lexeme(void)
+{
+    Lexeme *lexeme;
+    lexeme = malloc(sizeof(Lexeme));
+    if (!lexeme)
+    {
+        printf("error\n");
+        exit(1);
+    }
+
+    lexeme->str = malloc(LEXEME_STR_SIZE * sizeof(char));
+    if (!lexeme->str)
+    {
+        printf("error\n");
+        exit(1);
+    }
+
+    memset(lexeme->str, 0, LEXEME_STR_SIZE);
+    lexeme->str_len = 0;
+
+    return lexeme;
+}
+
+void
+append_lexeme_str(Lexeme *lexeme, char c)
+{
+    lexeme->str[lexeme->str_len] = c;
+    lexeme->str_len++;
+}
+
+Lexeme**
+lexemize(char *input)
+{
+    char *ptr;
+    Lexeme *lexeme, **arr;
+    size_t num_lexemes;
+
+    arr = malloc(sizeof(Lexeme*) * 256);
+    if (!arr)
+    {
+        printf("error\n");
+        exit(1);
+    }
+
+    num_lexemes = 0;
+    lexeme = init_lexeme();
+    for (ptr = input; *ptr != '\0'; ptr++)
+    {
+        if (*ptr == ' ')
+        {
+            arr[num_lexemes] = lexeme;
+            num_lexemes++;
+            lexeme = init_lexeme();
+            continue;
+        }
+
+        append_lexeme_str(lexeme, *ptr);
+    }
+
+    arr[num_lexemes] = lexeme;
+    return arr;
+}
+
 int
 main(void)
 {
+    Lexeme **arr;
+    size_t i;
+
+    arr = lexemize("int main(void) { return 0; }");
+    for (i = 0; i < 6; i++)
+    {
+        printf("%s\n", arr[i]->str);
+    }
+    return 0;
 }
