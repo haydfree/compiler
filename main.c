d604102aee14ccd72e3e4c33894ddd3bc58f9f15
Hayden Freeman <haydenfreeman@yahoo.com>
2025-04-24 09:23:30 -0500

    AUTO COMMIT: 2025-04-24 09:23:30

--- main.c
+++ main.c
@@ -1,33 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-void
-lexical_analysis(char *statement)
-{
-    char *ptr, **tokens;
-    size_t i,j;
-    tokens = malloc(255);
-    if (!tokens) 
-    { 
-        fprintf(stderr, "%s:%d ERROR: malloc failed\n", __FILE__, __LINE__);
-        exit(1);
-    }
-    for (ptr=strtok(statement, " "),i=0; ptr != NULL; ptr=strtok(NULL, " "),i++)
-    {
-        tokens[i] = strdup(ptr);
-        printf("%s\n", tokens[i]);
-    }
-    for (j = 0; j < i; j++)
-    {
-        free(tokens[j]);
-    }
-    free(tokens);
-}
+#include <ctype.h>
 
 int
 main(void)
 {
-    lexical_analysis("hello buddy guy"); 
-    return 0;
 }
